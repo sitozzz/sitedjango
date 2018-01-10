10e35ba4badf38fc1bbda4a07b188345b0a804d9
sitozzz <sit7602@gmail.com>
2018-01-10 23:22:32 +0500

    Передача данных в представление

--- example/views.py
+++ example/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'example/home.html')
+
+def contact(request):
+    return render(request, 'example/contact.html', {'values': ['Свяжитесь с нами!', '8-800-555-35-35']})
