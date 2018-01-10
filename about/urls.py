118992252f60b43b261f3e8649cfd703305831ba
sitozzz <sit7602@gmail.com>
2018-01-11 00:25:16 +0500

    Панель администратора + миграции

--- about/urls.py
+++ about/urls.py
@@ -3,7 +3,6 @@ from django.views.generic import ListView, DetailView
 from about.models import Articles
 
 urlpatterns = [
-    url(r'^$', ListView.as_view(queryset=Articles.objects.all().order_by("-date")[:10],
-    template_name="about/posts.html")),
+    url(r'^$', ListView.as_view(queryset=Articles.objects.all().order_by("-pub_date")[:10], template_name="about/posts.html")),
     
 ]
