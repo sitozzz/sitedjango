ad196ae407897dd1eb02a186e6bf4f098802a5ef
sitozzz <sit7602@gmail.com>
2018-01-11 12:51:28 +0500

    Переход на детальный просмотр

--- about/urls.py
+++ about/urls.py
@@ -4,5 +4,5 @@ from about.models import Articles
 
 urlpatterns = [
     url(r'^$', ListView.as_view(queryset=Articles.objects.all().order_by("-pub_date")[:10], template_name="about/posts.html")),
-    
+    url(r'^(?P<pk>\d+)$', DetailView.as_view(model = Articles,template_name="about/post.html")),
 ]
