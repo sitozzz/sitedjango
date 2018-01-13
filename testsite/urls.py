0b90db74e6634f9a091e8746a296a3ef35c9f3a2
sitozzz <sit7602@gmail.com>
2018-01-13 18:31:59 +0500

    форма обратной связи

--- testsite/urls.py
+++ testsite/urls.py
@@ -17,12 +17,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from registration.forms import RegistrationFormUniqueEmail
 from registration.views import RegistrationView
-
+from example.views import FeedBackForm, contactView, thnks
 urlpatterns = [    
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.urls')),
     url(r'^register/$', RegistrationView.register, {'form': RegistrationFormUniqueEmail}, name='registration_register'),
     url('', include('registration.urls')),    
     url(r'^', include('example.urls')),
+    url(r'^feedback/$', contactView, name='contact'),
+    url(r'^thnks/$', thnks, name='thnks'), 
     url(r'^about/', include('about.urls')),
 ]
